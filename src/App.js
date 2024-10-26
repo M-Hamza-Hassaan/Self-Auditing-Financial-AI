f24b45e0a8ceb448a2a0a0c14f5c12ff540caf7e
M_Hamza_Hassaan <mhhassaan.1@gmail.com>
2024-10-26 12:49:11 +0500

    I started from Here

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Format from './Pages/Format/Format';
+
+function App() {
+  return (
+    <div>
+      <h1>THIS IS THE CHESS AI APP</h1>
+      <Format />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Format/Format.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Format() {
+  return (
+    <>
+    in this format, i will add navbar and sidebar, header, main body which will be having al the dynamics of the app and then i will add footer to this format.
+    <br />
+    lets get started.
+    </>
+  )
+}
